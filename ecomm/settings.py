488e936faf783c471e1cd430abeec3dd703df496
anujdarji2001 <anujdarji2001@gmail.com>
2021-07-20 18:57:16 +0530

    Some Change

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','sparkcart.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','127.0.0.1','sparkcart.herokuapp.com']
 
 
 # Application definition
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
