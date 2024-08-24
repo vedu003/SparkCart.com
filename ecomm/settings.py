5602cb9e273333496134f72ae76e27bd91ecf360
Vedant Modi <85004625+vedu003@users.noreply.github.com>
2024-08-24 17:30:57 +0530

    Update settings.py

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','sparkcart.herokuapp.com']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
